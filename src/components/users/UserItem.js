a64c003888990e8f578faf266d2ec2a7b12fa9dd
Afonso Santos <afonso.faisca1@gmail.com>
2021-09-22 18:18:21 +0100

    Video 13

---
+++ src/components/users/UserItem.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const UserItem = ({ user: { avatar_url, login, html_url } }) => {
+  return (
+    <div className='card text-center'>
+      <img src={avatar_url} className='round-img' style={{ width: '60px' }} />
+      <h3>{login}</h3>
+      <div>
+        <a className='btn btn-dark btn-sm my-1' href={html_url}>
+          More
+        </a>
+      </div>
+    </div>
+  );
+};
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired,
+};
+
+export default UserItem;
